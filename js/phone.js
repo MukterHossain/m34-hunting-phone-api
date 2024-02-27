a2bc855921b9e71b6096b440351dc6e73eb76403
MukterHossain <mukterhossain3075@gmail.com>
2024-02-27 21:50:03 +0600

    added some new section

---
+++ js/phone.js
@@ -0,0 +1,82 @@
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    // const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const  data = await res.json();
+    const phones = data.data;
+    // console.log(phones)
+    displayPhones(phones)
+}
+
+const displayPhones = phones => {
+    // console.log(phones)
+    // 1: get id 
+    const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
+
+    // display show all button if there more than 12 phones
+    const showAllContainer = document.getElementById('show-all-container');
+    if(phones.length > 12){
+        showAllContainer.classList.remove('hidden');
+    }
+    else{
+        showAllContainer.classList.add('hidden')
+    }
+
+    //display only first 12 phones
+    phones = phones.slice(0, 12)
+
+    phones.forEach(phone =>{
+        console.log(phone)
+        // 1: create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card p-4 bg-gray-200 shadow-xl`;
+        // 3: set innerHTML
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>      
+        `;
+
+        // 4; append child
+        phoneContainer.appendChild(phoneCard)
+    });
+
+    //hide loading spinner
+    toggleLoadingSpinner(false);
+}
+
+// handle search button
+const handleSearch = () => {
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText)
+    loadPhone(searchText)
+}
+
+//handle search recap
+const handleSearch2 = () =>{
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field2');
+    const searchText = searchField.value;
+    loadPhone(searchText)
+}
+
+const toggleLoadingSpinner = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden')
+    }
+    else{
+        loadingSpinner.classList.add('hidden')
+    }
+} 
+
+
+// loadPhone()
